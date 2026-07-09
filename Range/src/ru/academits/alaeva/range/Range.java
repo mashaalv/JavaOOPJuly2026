be53ee513b68db29b598b72e9a77c06a08f75baf
mashaalv <mashaalv@gmail.com>
2026-07-10 02:07:53 +0700

    Add Range project

---
+++ Range/src/ru/academits/alaeva/range/Range.java
@@ -0,0 +1,4 @@
+package ru.academits.alaeva.range;
+
+public class Range {
+}
---
+++ Range/src/ru/academits/alaeva/range_main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.alaeva.range_main;
+
+public class Main {
+    static void main() {
+        System.out.println("Hello word");
+    }
+}
