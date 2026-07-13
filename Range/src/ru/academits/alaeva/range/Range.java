af0efcfb2a46a6e4da75405c9f4b7d28745fc5db
mashaalv <mashaalv@gmail.com>
2026-07-13 15:33:30 +0700

    New changes.

--- Range/src/ru/academits/alaeva/range/Range.java
+++ Range/src/ru/academits/alaeva/range/Range.java
@@ -1,8 +1,87 @@
 package ru.academits.alaeva.range;
 
-
 public class Range {
-    static void main() {
-        System.out.println("hello world1");
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double pointX) {
+        return from <= pointX && pointX <= to;
+    }
+
+
+    public double max(double number1, double number2) {
+        return number2 > number1 ? number2 : number1;
+    }
+
+    public double min(double number1, double number2) {
+        return number2 < number1 ? number2 : number1;
+    }
+
+    public Range intersection(Range anotherRange) {
+        // если нет пересечения, возвращаем null, иначе пересечение
+        if (max(this.from, anotherRange.from) >= min(this.to, anotherRange.to)) {
+            return null;
+        }
+        return new Range(max(this.from, anotherRange.from), min(this.to, anotherRange.to));
+    }
+
+    public Range[] union(Range anotherRange) {
+        // если нет пересечения, возвращаем 2 интервала, иначе 1
+        if (max(this.from, anotherRange.from) > min(this.to, anotherRange.to)) {
+
+            if (this.from < anotherRange.from) {
+                return new Range[]{this, anotherRange};
+            } else {
+                return new Range[]{anotherRange, this};
+            }
+        }
+        return new Range[]{new Range(min(this.from, anotherRange.from), max(this.to, anotherRange.to))};
+    }
+
+    public Range[] difference(Range anotherRange) {
+        // нет пересечений, возвращаем первый интервал
+        if (max(this.from, anotherRange.from) > min(this.to, anotherRange.to)) {
+            return new Range[]{this};
+        }
+        // есть пересечения, a1<a2, b2>b1 -- [a1,a2]
+        if (anotherRange.from > this.from && anotherRange.to > this.to) {
+            return new Range[]{new Range(this.from, anotherRange.from)};
+        }
+        //есть пересечения, a2<a1, b2<b1 -- [b2,b1]
+        if (anotherRange.from < this.from && anotherRange.to < this.to) {
+            return new Range[]{new Range(anotherRange.to, this.to)};
+        }
+        //есть пересечения, a2>a1, b2<b1 --[a1,a2] и [b2,b1]
+        if (anotherRange.from > this.from && anotherRange.to < this.to) {
+            return new Range[]{new Range(this.to, anotherRange.to), new Range(anotherRange.from, this.from)};
+        }
+        // иначе второй интервал включает первый - ноль
+        return new Range[0];
     }
 }
--- Range/src/ru/academits/alaeva/range_main/Main.java
+++ Range/src/ru/academits/alaeva/range_main/Main.java
@@ -1,7 +1,94 @@
 package ru.academits.alaeva.range_main;
 
+import ru.academits.alaeva.range.Range;
+
 public class Main {
-    static void main() {
-        System.out.println("Hello word");
+    public static void printIfInside(double pointX, Range range) {
+
+        if (range.isInside(pointX)) {
+            System.out.println("Точка " + pointX + " принадлежит диапазону [" + range.getFrom() + "," + range.getTo() + "]");
+
+        } else {
+            System.out.println("Точка " + pointX + " не принадлежит диапазону [" + range.getFrom() + "," + range.getTo() + "]");
+        }
+    }
+
+    public static void printLength(Range range) {
+        System.out.println("Длина диапазона [" + range.getFrom() + "," + range.getTo() + "] = " + range.getLength());
+    }
+
+    // вывод диапазона
+    public static void printRange(Range range) {
+        if (range == null) {
+            System.out.println("Range is null.");
+            return;
+        }
+        System.out.println("Диапазон [" + range.getFrom() + "," + range.getTo() + "]");
+    }
+
+    // вывод для массива диапазонов
+    public static void printRanges(Range[] range) {
+        if (range.length == 0) {
+            System.out.println("[]");
+            return;
+        }
+        for (int i = 0; i < range.length; i++) {
+            printRange(range[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        // не пересекаются
+        Range range1 = new Range(1.5, 10.3);
+        Range range2 = new Range(-8, -3);
+        //пересекаются
+        Range range3 = new Range(1, 10);
+        Range range4 = new Range(8, 12);
+        //пересекаются по одной точке
+        Range range5 = new Range(5, 10);
+        Range range6 = new Range(10, 12);
+
+        // тестирование с range1
+        printLength(range1);
+        printIfInside(-1, range1);
+        range1.setFrom(-1);
+        System.out.println("После изменения поля через сеттер:");
+        printIfInside(-1, range1);
+        printLength(range1);
+
+        // тестирование с range 2
+        printLength(range2);
+        printIfInside(0, range2);
+        range2.setTo(0);
+        System.out.println("После изменения поля через сеттер:");
+        printIfInside(0, range2);
+        printLength(range2);
+
+        // тестирование union
+        System.out.println();
+        System.out.println("Тестирование union:");
+
+        Range[] unionRange3 = range3.union(range4);
+        printRanges(unionRange3);
+        Range[] unionRange5 = range5.union(range6);
+        printRanges(unionRange5);
+
+        // тестирование intersection
+        System.out.println();
+        System.out.println("Тестирование intersection:");
+        Range intersectionRange3 = range3.intersection(range4);
+        printRange(intersectionRange3);
+        Range intersectionRange5 = range5.intersection(range6);
+        printRange(intersectionRange5);
+
+        // тестирование difference
+        System.out.println();
+        System.out.println("Тестирование difference:");
+        Range differenceRange3 = range3.intersection(range4);
+        printRange(differenceRange3);
+        Range differenceRange5 = range5.intersection(range6);
+        printRange(differenceRange5);
+
+
     }
 }
