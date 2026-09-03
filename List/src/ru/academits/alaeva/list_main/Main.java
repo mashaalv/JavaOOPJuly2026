520610ae6ef3ef3bb62a87a7afef4a99b40f27d5
MariaAlaeva <mashaalv@gmail.com>
2026-09-03 22:04:09 +0700

    Задача ArrayListHome.

---
+++ List/src/ru/academits/alaeva/list_main/Main.java
@@ -0,0 +1,19 @@
+package ru.academits.alaeva.list_main;
+
+import ru.academits.alaeva.list.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new List<>();
+
+        list.getData(0);
+
+
+
+
+
+
+    }
+}
+
+
