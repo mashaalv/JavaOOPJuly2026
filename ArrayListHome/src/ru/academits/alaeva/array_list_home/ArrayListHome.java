983f42038dfa41183883bf2b6954348741369acf
MariaAlaeva <mashaalv@gmail.com>
2026-09-03 16:56:05 +0700

    Задача ArrayListHome.

--- ArrayListHome/src/ru/academits/alaeva/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/alaeva/array_list_home/ArrayListHome.java
@@ -1,23 +1,32 @@
 package ru.academits.alaeva.array_list_home;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListHome {
+    // 1. Прочитать в список все строки из файла
+    public static List<String> readLinesFromFile(String fileName) throws IOException {
+        // создаем список для строк из файла:
+        List<String> lines = new ArrayList<>();
+        // читаем построчно из файла:
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
 
     // 2. Есть список из целых чисел. Удалить из него все четные числа. В этой задаче новый список создавать нельзя
     public static void removeEvenNumbers(ArrayList<Integer> numbers) {
-
-       /* for (Integer number : numbers) {
-            if(number%2==0){
-                numbers.remove(number);
-            }
-        }//так делать нельзя*/
-
         // итератор для списка
         Iterator<Integer> iterator = numbers.iterator();
-
 
         while (iterator.hasNext()) {
             Integer currentNumber = iterator.next();
@@ -26,7 +35,17 @@ public class ArrayListHome {
                 iterator.remove();
             }
         }
-
     }
 
+    // 3.Есть список из целых чисел, в нём некоторые числа могут повторяться.
+    // Cоздать новый список, в котором будут элементы первого списка в таком же порядке, но без повторений
+    public static List<Integer> getUniqueNumbers(ArrayList<Integer> numbers) {
+        List<Integer> resultList = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (!resultList.contains(number)) {
+                resultList.add(number);
+            }
+        }
+        return resultList;
+    }
 }
--- ArrayListHome/src/ru/academits/alaeva/array_list_home_main/Main.java
+++ ArrayListHome/src/ru/academits/alaeva/array_list_home_main/Main.java
@@ -2,42 +2,35 @@ package ru.academits.alaeva.array_list_home_main;
 
 import ru.academits.alaeva.array_list_home.ArrayListHome;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. тест Часть1 ======= Прочитать в список все строки из файла
-        // создаем список для строк из файла:
-        ArrayList<String> arraylistLines = new ArrayList<>();
-
-        // читаем построчно из файла:
-        try (BufferedReader reader = Files.newBufferedReader(Path.of("input.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                arraylistLines.add(line);
-            }
-            // Выводим результат
+        // 1. тест Часть1 =======
+        try {
+            List<String> lines = ArrayListHome.readLinesFromFile("input1.txt");
             System.out.println("Содержимое файла:");
-            System.out.println(arraylistLines);
-
+            System.out.println(lines);
         } catch (IOException e) {
-            //throw new RuntimeException(e);
             System.out.println("Ошибка при чтении файла: " + e.getMessage());
         }
-
         // 2. тест Часть 2 ===========
-        ArrayList<Integer> numbers=new ArrayList<>(Arrays.asList(1, 54, -8, 10, 3, 4,9,5,666,667));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 54, -8, 10, 3, 4, 9, 5, 666, 667));
         System.out.println("Список до удаления четных чисел:");
         System.out.println(numbers);
         ArrayListHome.removeEvenNumbers(numbers);
         System.out.println("Список после удаления четных чисел:");
         System.out.println(numbers);
 
+        // 3. тест Часть 3 ===========
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(4, 52, 4, 89, -1, 1, 1, 2, 2, 2, 3, 4, 1, 2, 5, 4));
+        System.out.println("Список до удаления повторений");
+        System.out.println(list);
+
+        System.out.println("Список после удаления повторений");
+        System.out.println(ArrayListHome.getUniqueNumbers(list));
     }
 }
